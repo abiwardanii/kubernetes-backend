2bb6784b05674694a2d1162c091d00566746e695
Arif H <arifh.py@gmail.com>
2020-11-13 16:54:04 +0700

    port db redis

--- src/config/Databases.js
+++ src/config/Databases.js
@@ -4,7 +4,8 @@ const myDb = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 module.exports = myDb
