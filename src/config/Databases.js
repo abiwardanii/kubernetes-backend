34ce4b820870229d609ad4f38778c770d9d94841
Arif Hidayat <arif.h@mail.ugm.ac.id>
2020-11-26 07:47:03 +0700

    port pg

--- src/config/Databases.js
+++ src/config/Databases.js
@@ -5,7 +5,7 @@ const myDb = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    port: 21999
+    port: process.env.DB_PORT
 })
 
 module.exports = myDb
